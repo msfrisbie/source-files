1405868a35f4f68ef5bec80b55567cb546975385
Matt Frisbie <msfrisbie@gmail.com>
2015-08-11 08:13:29 -0700

    meteor and angular updates

---
+++ Learning AngularJS 2.0 and Next Generation JavaScript/7-Meteor/0703 Publish-Subscribe Model/demo.js
@@ -0,0 +1,45 @@
+Players = new Mongo.Collection("players");
+
+
+Meteor.methods({
+  addPlayer: function(player) {
+    Players.insert({
+      name: player.name,
+      number: player.number,
+      position: player.position
+    })
+  }
+})
+
+if (Meteor.isClient) {
+  Meteor.subscribe("player-subset");
+
+  Template.body.helpers({
+    players: function() {
+      // Get all players in the db
+      return Players.find({});
+    }
+  });
+
+  Template.body.events({
+    "submit #create-player": function(event) {
+      event.preventDefault();
+
+      Meteor.call("addPlayer", {
+        name: event.target.name.value,
+        number: event.target.number.value,
+        position: event.target.position.value
+      });
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.publish("player-subset", function() {
+    return Players.find({
+      name: {
+        $ne: "Aaron Rodgers"
+      }
+    });
+  });
+}
